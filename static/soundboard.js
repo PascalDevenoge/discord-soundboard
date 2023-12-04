4c5312936587c037fd5d86d00c78afdb89930cd3
takotori <tranfelix99@gmail.com>
2023-12-04 19:55:23 +0100

    Tracks are now sortable Add mobile friendly design

--- static/soundboard.js
+++ static/soundboard.js
@@ -1,38 +1,98 @@
-let soundContainer = document.getElementById("soundBites");
 let submitUpload = document.getElementById('submitUpload');
+let favorites = document.getElementById('favorites');
+let remainder = document.getElementById('remainder');
+let canSortButton = document.getElementById('isSortable');
+
+let isLocked = true;
+let favoritesArr = JSON.parse(localStorage.getItem('favoritesArr'));
+let remainderArr = JSON.parse(localStorage.getItem('remainderArr'));
+
+let favoritesSort = new Sortable(favorites, {
+    group: 'shared',
+    filter: '.title',
+    animation: 150,
+    disabled: true
+});
+
+let remainderSort = new Sortable(remainder, {
+    group: 'shared',
+    filter: '.title',
+    animation: 150,
+    disabled: true
+});
 
 tracks = fetch('tracks')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Failed to load available tracks")
-    }
-    return response.json()
-  })
-  .then(tracks => {
-    for (let id in tracks) {
-      let track_name = tracks[id];
-      let button = document.createElement("button", {id: `${track_name}_button`})
-      button.innerText = track_name
-      button.classList.add("list-group-item,list-group-item-action");
-
-      soundContainer.appendChild(button);
-      button.addEventListener("click", () => {
-        fetch(`play/${id}`)
-      }, false)
-    }
-
-    let nuclear_button = document.getElementById('nuclear_option');
-    nuclear_button.addEventListener("click", () => {
-      fetch(`play/all`)
-    }, false)
-
-    let stop_button = document.getElementById('stop')
-    stop_button.addEventListener("click", () => {
-      fetch("stop")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Failed to load available tracks")
+        }
+        return response.json()
     })
-  })
+    .then(tracks => {
+        for (let track of favoritesArr) {
+            let button = createButton(track);
+            favorites.appendChild(button);
+        }
+
+        for (let track of remainderArr) {
+            let button = createButton(track);
+            remainder.appendChild(button);
+        }
+
+        let newTracks = tracks.filter(item => !favoritesArr.includes(item)).filter(item => !remainderArr.includes(item));
+        for (let track of newTracks) {
+            let button = createButton(track);
+            remainder.appendChild(button);
+        }
+
+        let nuclear_button = document.getElementById('nuclear_option');
+        nuclear_button.addEventListener("click", () => {
+            fetch(`play/all`)
+        }, false)
+
+        let stop_button = document.getElementById('stop')
+        stop_button.addEventListener("click", () => {
+            fetch("stop")
+        })
+    })
+
+function createButton(track) {
+    let button = document.createElement("a", {id: `${track}_button`});
+    button.innerText = track;
+    button.classList.add("list-group-item", "list-group-item-action", "soundBite");
+
+    button.addEventListener("click", () => {
+        fetch(`play/${track}`)
+    }, false);
+    return button;
+}
 
 submitUpload.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("aaaaaaaaaaaaaaaaaaaa");
+    e.preventDefault();
+    console.log("aaaaaaaaaaaaaaaaaaaa");
+});
+
+canSortButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (isLocked) {
+        canSortButton.textContent = "Lock Order";
+        isLocked = false;
+
+        favoritesSort.option('disabled', false);
+        remainderSort.option('disabled', false);
+    } else {
+        canSortButton.textContent = "Unlock Order";
+        isLocked = true;
+
+        favoritesSort.option('disabled', true);
+        remainderSort.option('disabled', true);
+
+        console.log(favoritesSort.options);
+
+        let favoritesArr = [...favorites.children].filter(child => child.tagName === 'A').map(child => child.text);
+        let remainderArr = [...remainder.children].filter(child => child.tagName === 'A').map(child => child.text);
+
+        localStorage.setItem('favoritesArr', JSON.stringify(favoritesArr));
+        localStorage.setItem('remainderArr', JSON.stringify(remainderArr));
+    }
 });
