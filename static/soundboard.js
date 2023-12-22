61d168bc29c8065143986c2f23f75f1c6b301d6b
takotori <tranfelix99@gmail.com>
2023-12-22 17:51:21 +0100

    extract contextmenu

--- static/soundboard.js
+++ static/soundboard.js
@@ -1,16 +1,14 @@
-import {getTracks, playAllTracks, stopTracks, deleteTrack, renameTrack} from "./api.js";
+import {getTracks, playAllTracks, stopTracks} from "./api.js";
 import {createButton} from "./domBuilder.js";
-import {getFavorites, removeFavorite, removeVolume, setFavorites} from "./storage.js";
+import {getFavorites, setFavorites} from "./storage.js";
+import {initContextMenu} from "./contextMenu/contextMenu.js";
 
 let favorites = document.getElementById('favorites');
 let remainder = document.getElementById('remainder');
 let canSortButton = document.getElementById('sortUnlockButton');
 let nuclearButton = document.getElementById('playAllButton');
 let stopButton = document.getElementById('stopButton')
-let uploadImageButton = document.getElementById('submitImageUpload')
-let renameTrackButton = document.getElementById('submitTrackName')
-let deleteTrackButton = document.getElementById('deleteTrack')
-let trackImage = document.getElementById('trackImage')
+
 
 let favoritesSort = new Sortable(favorites, {
     group: 'shared',
@@ -50,56 +48,6 @@ getTracks()
         }
     });
 
-let selectedTrackUUID = "";
-let selectedTrack;
-
-window.addEventListener("contextmenu", function (e) {
-    let target = e.target;
-    while (target !== null) {
-        if (target.classList && target.classList.contains('soundBite')) {
-            e.preventDefault();
-            let contextMenu = document.getElementById("contextMenu");
-            contextMenu.style.top = `${e.pageY}px`;
-            contextMenu.style.left = `${e.pageX}px`;
-            contextMenu.removeAttribute("hidden");
-            selectedTrackUUID = target.getAttribute("data-uuid");
-            selectedTrack = target;
-            return;
-        }
-        target = target.parentNode;
-    }
-});
-
-window.addEventListener("click", function (e) {
-    document.getElementById("contextMenu").setAttribute("hidden", "true");
-});
-
-uploadImageButton.addEventListener("click", (e) => {
-    let image = trackImage.files[0];
-});
-
-renameTrackButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (selectedTrackUUID === "") {
-        return;
-    }
-    let newName = document.getElementById("trackName").value;
-    renameTrack(selectedTrackUUID, newName);
-    selectedTrack.getElementsByTagName("p")[0].textContent = newName;
-    bootstrap.Modal.getInstance(document.getElementById('renameTrackDialog')).hide();
-});
-
-deleteTrackButton.addEventListener("click", (e) => {
-    if (selectedTrackUUID === "") {
-        return;
-    }
-    selectedTrack.remove();
-    console.log(selectedTrack);
-    deleteTrack(selectedTrackUUID);
-    removeFavorite(selectedTrackUUID);
-    removeVolume(selectedTrackUUID);
-});
-
 nuclearButton.addEventListener("click", () => {
     playAllTracks();
 });
@@ -126,3 +74,5 @@ canSortButton.addEventListener("click", (e) => {
         setFavorites(favOrder);
     }
 });
+
+initContextMenu();
