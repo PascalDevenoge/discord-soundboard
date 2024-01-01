d20d3cb7872613fba2126bb229044c58f6f23a21
Pascal Devenoge <pascal@devenoge.net>
2024-01-01 17:42:22 +0100

    Slightly rearranges UI and adds search field

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -63,16 +63,18 @@ const settingsDialogTrackTitle = computed(() => 'Settings for ' + TrackRepositor
 </script>
 
 <template>
-  <div class="relative">
+  <div>
     <p class="text-4xl md:text-6xl italic text-center py-6 md:py-12">The ultimate Discord Soundboard</p>
     <div class="flex md:flex-row flex-col justify-center">
       <HeaderButton @click="backendRequest('/play/all')">The nuclear option</HeaderButton>
       <HeaderButton @click="backendRequest('/stop')" :highlighted="true">Stop all</HeaderButton>
       <HeaderButton @click="uploadDialogOpen = true">Upload</HeaderButton>
     </div>
-    <div class="pt-5 grid justify-center grid-cols-1 md:grid-cols-2 gap-10">
+    <div class="pt-16 grid justify-center grid-cols-1 md:grid-cols-1 gap-10">
+      <hr class="h-px border-x border-gray-300"/>
       <SoundboardContainer @buttonRightClick="buttonRightClickHandler" :name="'Favorites'" :selection-predicate="(id) => TrackRepository.getTrack(id).favorite"/>
-      <SoundboardContainer @buttonRightClick="buttonRightClickHandler" :name="'Uncategorized'" :selection-predicate="(id) => !TrackRepository.getTrack(id).favorite" />
+      <hr class="h-px border-x border-gray-300"/>
+      <SoundboardContainer @buttonRightClick="buttonRightClickHandler" :name="'Uncategorized'" :selection-predicate="(id) => !TrackRepository.getTrack(id).favorite" :searchable="true"/>
     </div>
     <ModalDialog v-if="uploadDialogOpen" @close="uploadDialogOpen = false" :title="'Upload clip'" :ok-action="uploadFile">
       <input id="uploadClipFile" class="block w-full h-10 border border-gray-300 rounded-lg file:rounded-l-lg file:border-none file:text-white file:font-semibold file:bg-blue-600 file:hover:bg-blue-500 file:h-10 file:w-24 file:mr-3" type="file"/>
@@ -82,5 +84,9 @@ const settingsDialogTrackTitle = computed(() => 'Settings for ' + TrackRepositor
       <label for="default-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Volume</label>
       <input id="default-range" type="range" min="-30" max="30" value="0" step="0.1" class="">
     </ModalDialog>
+
+    <div>
+      <p class="float-right m-4 text-gray-500">© {{ new Date().getFullYear() }} Pascal Devenoge, Felix Tran</p>
+    </div>
   </div>
 </template>
--- frontend/src/components/SoundboardContainer.vue
+++ frontend/src/components/SoundboardContainer.vue
@@ -1,23 +1,32 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import TrackRepository from '../assets/TrackRepository'
 import SoundboardButton from './SoundboardButton.vue'
 
 const props = defineProps({
   name: String,
-  selectionPredicate: Function
+  selectionPredicate: Function,
+  searchable: {
+    default: false,
+    type: Boolean
+  }
 })
 
 const eventEmit = defineEmits('buttonRightClick')
 
+const searchTerm = ref('')
+
 const selectedTracks = computed(() => {
-  return TrackRepository.getAllTrackIds().filter(id => props.selectionPredicate(id))
+  return TrackRepository.getAllTrackIds().filter(id => props.selectionPredicate(id)).filter(id => TrackRepository.getTrack(id).name.toLowerCase().includes(searchTerm.value.toLowerCase()))
 })
 </script>
 
 <template>
-  <div>
-    <p class="text-3xl text-center pb-8">{{ props.name }}</p>
+  <div class="mb-5">
+    <p class="text-3xl text-center mb-8">{{ props.name }}</p>
+    <div class="flex justify-center">
+      <input v-if="searchable" v-model="searchTerm" placeholder="Search for clip name..." class="align-middle w-80 h-10 text-lg mb-6 py-2 px-4 border border-gray-300 rounded-xl">
+    </div>
     <div class="flex flex-col md:flex-row justify-center flex-wrap">
       <SoundboardButton @rightClick="(id, x, y) => eventEmit('buttonRightClick', id, x, y)" v-for="id of selectedTracks" :key="id" :trackId="id"/>
     </div>
